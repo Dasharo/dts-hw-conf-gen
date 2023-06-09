03173f36f91b0bc1fe28b9134b549cb986cb7641
Piotr Król <piotr.krol@3mdeb.com>
2023-06-09 14:16:21 +0200

    add force implementation
    Signed-off-by: Piotr Król <piotr.krol@3mdeb.com>

--- src/cpu_command.sh
+++ src/cpu_command.sh
@@ -1,39 +1,11 @@
 # Variable containing the path to the tar.gz file
 hcl_report=${args[hcl_report]}
+force=${args[--force]}
 
 # Extract the directory name from the file path
 dir_name=$(basename "$hcl_report" .tar.gz)
 
-# Check if the directory already exists
-if [ -d "$dir_name" ]; then
-    # Warn the user that the directory exists
-    echo "Warning: Directory $dir_name already exists."
-
-    # Ask for confirmation to remove the directory
-    read -p "Do you want to remove the directory and unpack the file again? (y/n) " confirm
-    if [ "$confirm" == "y" ] || [ "$confirm" == "Y" ]; then
-        # Remove the directory
-        rm -rf "$dir_name"
-
-        # Create the directory
-        mkdir "$dir_name"
-
-        # Unpack the tar.gz file into the directory
-        tar -xzf "$hcl_report" -C "$dir_name"
-
-        echo "Directory removed and file $hcl_report unpacked."
-    else
-        echo "Operation aborted."
-    fi
-else
-    # If the directory does not exist, create the directory
-    mkdir "$dir_name"
-
-    # If the directory does not exist, unpack the tar.gz file
-    tar -xzf "$hcl_report" -C "$dir_name"
-
-    echo "File $hcl_report unpacked."
-fi
+perform_extraction "$dir_name" "$force" "$hcl_report"
 
 # Extract the Dasharo Version using the function
 dasharo_version=$(extract_dasharo_version "$dir_name/logs/dmidecode.log")
@@ -48,4 +20,6 @@ cpu_model=$(grep 'model name' "$cpuinfo_file" | head -1 | awk -F ': ' '{print $2
 # Report
 source="Dasharo HCL Report"
 
-echo "| $cpu_model | $dasharo_version | $source |"
+if [ -n "$dasharo_version" ];then
+	echo "| $cpu_model | $dasharo_version | $source |"
+fi
--- src/lib/dasharo.sh
+++ src/lib/dasharo.sh
@@ -11,3 +11,31 @@ extract_dasharo_version() {
   echo "$dasharo_version"
 }
 
+perform_extraction() {
+    local dir_name=$1
+    local force=$2
+    local hcl_report=$3
+
+    # Check if the directory already exists
+    if [ -d "$dir_name" ]; then
+        # Remove the directory if force flag is set or ask for confirmation
+        if [ "$force" = "1" ]; then
+            rm -rf "$dir_name"
+        else
+            echo "Warning: Directory $dir_name already exists."
+            read -p "Do you want to remove the directory and unpack the file again? (y/n) " confirm
+            if [ "$confirm" != "y" ] && [ "$confirm" != "Y" ]; then
+                echo "Operation aborted."
+                return 0
+            fi
+            rm -rf "$dir_name"
+        fi
+    fi
+
+    # Create the directory
+    mkdir -p "$dir_name"
+
+    # Unpack the tar.gz file into the directory
+    tar -xzf "$hcl_report" -C "$dir_name"
+}
+
