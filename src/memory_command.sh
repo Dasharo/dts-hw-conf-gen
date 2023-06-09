5db70d4aaaa4aad60db58e945fffaf5f22bdc1c4
Piotr Król <piotr.krol@3mdeb.com>
2023-06-09 19:01:22 +0200

    src/memory_command.sh: implement dasharo hcl parsing
    Signed-off-by: Piotr Król <piotr.krol@3mdeb.com>

--- src/memory_command.sh
+++ src/memory_command.sh
@@ -1,4 +1,57 @@
-echo "# this file is located in 'src/memory_command.sh'"
-echo "# code for 'dts-hclmgr memory' goes here"
-echo "# you can edit it freely and regenerate (it will not be overwritten)"
-inspect_args
+# Variable containing the path to the tar.gz file
+hcl_report=${args[hcl_report]}
+force=${args[--force]}
+quiet=${args[--quiet]}
+
+# Extract the directory name from the file path
+dir_name=$(basename "$hcl_report" .tar.gz)
+
+perform_extraction "$dir_name" "$force" "$hcl_report"
+
+# Extract the Dasharo Version using the function
+dasharo_version=$(extract_dasharo_version "$dir_name/logs/dmidecode.log")
+
+if [ ! -n "$dasharo_version" ];then
+	if [ "$quiet" != "1" ]; then
+		echo "ERROR: Vendor BIOS HCL"
+	fi
+	exit 1
+fi
+
+
+# File containing the decode-dimms
+decodedimms_file="$dir_name/logs/decode-dimms.log"
+
+if [ ! -f "$decodedimms_file" ]; then
+	if [ "$quiet" != "1" ]; then
+		echo "ERROR: Decode DIMMs does not extist: $decodedimms_file"
+	fi
+	exit 1
+fi
+
+file_contents=$(< $decodedimms_file)
+
+num_modules=$(grep -oP "(?<=Number of SDRAM DIMMs detected and decoded: )\d+" "$decodedimms_file")
+# Loop through each bank
+for ((bank=1; bank<=num_modules; bank++)); do
+
+  module_manufacturer=$(extract_lookup_string_from_decode_dimms $bank "$file_contents" "Module Manufacturer")
+  part_num=$(extract_lookup_string_from_decode_dimms $bank "$file_contents" "Part Number")
+  size=$(extract_lookup_string_from_decode_dimms $bank "$file_contents" "Size")
+  mem_type=$(extract_lookup_string_from_decode_dimms $bank "$file_contents" "Fundamental Memory type")
+  speed=$(extract_lookup_string_from_decode_dimms $bank "$file_contents" "Maximum module speed")
+
+  case $num_modules in
+    1)
+      conf="&#10004/-/-";;
+    2)
+      conf="-/&#10004/-";;
+    4)
+      conf="-/-/&#10004";;
+    *)
+      conf="unknown"
+  esac
+
+	echo "| $module_manufacturer | $part_num | $size | $mem_type | $speed | $conf | $dasharo_version | Dasharo HCL report |"
+done
+
